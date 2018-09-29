6c5b07e4b19019516c484f286760ccc30df1989a
xlui <liuqi0315@gmail.com>
2018-09-29 18:08:39 +0800

    Redis delayed task

---
+++ common/common_test.go
@@ -0,0 +1,7 @@
+package common
+
+import "testing"
+
+func TestFailOnError(t *testing.T) {
+	// nothing to test here!
+}
--- redis/redis.go
+++ redis/redis.go
@@ -1,36 +1,72 @@
 package redis
 
 import (
-	"fmt"
+	"eFuture/common"
 	"github.com/go-redis/redis"
+	"github.com/satori/go.uuid"
+	"log"
+	"time"
 )
 
-func main() {
-	client := redis.NewClient(&redis.Options{
+const QUEUE_KEY = "mail_queue"
+
+var client *redis.Client
+
+func init() {
+	client = redis.NewClient(&redis.Options{
 		Addr:     "127.0.0.1:6379",
 		Password: "",
 		DB:       0,
 	})
-	pong, e := client.Ping().Result()
-	fmt.Println(pong, e)
+	_, e := client.Ping().Result()
+	common.FailOnError(e, "Failed to connect to redis")
+	client.Del(QUEUE_KEY)
+}
 
-	e = client.Set("key", "val", 0).Err()
-	if e != nil {
-		panic(e)
+func Push(message string, date time.Time) {
+	id := uuid.NewV4().String()
+	client.Set(id, message, 0)
+	client.ZAdd(QUEUE_KEY, redis.Z{
+		Score:  float64(date.Unix()),
+		Member: id,
+	})
+}
+
+func Pop() (bool, string) {
+	task := client.ZRange(QUEUE_KEY, 0, 0).Val()
+	if len(task) == 0 {
+		return false, ""
 	}
-
-	val, e := client.Get("key").Result()
-	if e != nil {
-		panic(e)
-	}
-	fmt.Println("key:", val)
-
-	val2, e := client.Get("key2").Result()
-	if e == redis.Nil {
-		fmt.Println("key2 does not exist!")
-	} else if e != nil {
-		panic(e)
+	id := task[0]
+	current := int64(client.ZScore(QUEUE_KEY, id).Val())
+	if current <= time.Now().Unix() {
+		data, _ := client.Get(id).Result()
+		pipeline := client.Pipeline()
+		pipeline.ZRem(QUEUE_KEY, id)
+		pipeline.Del(id)
+		_, e := pipeline.Exec()
+		common.FailOnError(e, "Failed to exec delete")
+		return true, data
 	} else {
-		fmt.Println("key2:", val2)
+		return false, ""
 	}
+}
+
+func main() {
+	date1 := time.Now().Add(time.Second * 1)
+	date2 := time.Now().Add(time.Second * 2)
+	date3 := time.Now().Add(time.Second * 3)
+	log.Println("Push data 1")
+	Push("hello 1", date1)
+	log.Println("Push data 2")
+	Push("hello 2", date2)
+	log.Println("Push data 3")
+	Push("hello 3", date3)
+	for {
+		time.Sleep(1 * time.Second)
+		if b, d := Pop(); b {
+			log.Println(d)
+		}
+	}
+
 }
