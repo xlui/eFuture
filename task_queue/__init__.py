8b0d5d4cb8cabf287aa1a71643cd5ad1da3d442a
xlui <liuqi0315@gmail.com>
2018-10-05 14:07:26 +0800

    Fix unittest broken(cyclic dependence)

--- task_queue/__init__.py
+++ task_queue/__init__.py
@@ -2,10 +2,12 @@
 import redis
 
 from config import redisHost, redisPort, redisPassword, redisDB
-from .redis_impl import push, pop
 
 QUEUE_KEY = 'mail_queue'
 pool = redis.ConnectionPool(host=redisHost, port=redisPort, db=redisDB, password=redisPassword)
 connection = redis.Redis(connection_pool=pool)
 # delete the following line when deploy
 connection.delete(QUEUE_KEY)
+
+# To avoid circulate dependency
+from .redis_impl import push, pop
