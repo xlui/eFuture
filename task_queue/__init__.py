9f914c6ae822ba8a8ae3e32c9b19dbd934c111f2
xlui <liuqi0315@gmail.com>
2018-10-15 19:25:15 +0800

    For deployment, do't delete mail queue at start

--- task_queue/__init__.py
+++ task_queue/__init__.py
@@ -7,7 +7,7 @@ QUEUE_KEY = 'mail_queue'
 pool = redis.ConnectionPool(host=redisHost, port=redisPort, db=redisDB, password=redisPassword)
 connection = redis.Redis(connection_pool=pool)
 # delete the following line when deploy
-connection.delete(QUEUE_KEY)
+# connection.delete(QUEUE_KEY)
 
 # To avoid circulate dependency
 from .redis_impl import push, pop
